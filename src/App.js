320e1ff53dd2b064766f679da778f0b914437d91
Sergio Hortua <sergioandresjsv@gmail.com>
2022-05-25 16:31:05 -0500

    edit for web

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sergio editado
+          Sergio cambio realizado
         </p>
         <a
           className="App-link"
